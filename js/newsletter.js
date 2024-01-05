01786899ede0d8d2a1feb8159715138df2999689
Damuchi99 <damuchi99@gmail.com>
2024-01-05 17:51:15 -0300

    V2: Validación de email y mensaje de confirmación con email ingresado

--- js/newsletter.js
+++ js/newsletter.js
@@ -2,6 +2,11 @@ let email_imput = document.getElementById('input-email');
 
 let error_text = document.getElementById('error-text');
 
+let sign_up_form_section = document.getElementById('sign-up-form');
+let success_message_section = document.getElementById('success-message');
+
+let submitted_email = document.getElementById('submitted-email');
+
 document.getElementById('subscribe-button').addEventListener('click', function(){
     let email = email_imput.value;
     console.log(email);
@@ -13,7 +18,9 @@ document.getElementById('subscribe-button').addEventListener('click', function()
         email_imput.style.borderColor = "rgb(206, 212, 218)";
         email_imput.style.backgroundColor = "rgb(255, 255, 255)";
         error_text.innerHTML = "";
-		//cambiar de seccion
+		sign_up_form_section.style.display = "none";
+        success_message_section.style.display = "block";
+        submitted_email.innerHTML = email;
 	}else{
         email_imput.style.color = "red";
         email_imput.style.borderColor = "rgb(255, 87, 87)";
